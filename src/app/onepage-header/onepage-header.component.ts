58dc798abf1d936692cb320dc4d7672b728c46ad
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-01-19 16:30:26 +0300

    header

--- src/app/onepage-header/onepage-header.component.ts
+++ src/app/onepage-header/onepage-header.component.ts
@@ -1,11 +1,16 @@
-//import
+// CommonModule
 import { CommonModule } from '@angular/common';
+
+// Core
 import { Component } from '@angular/core';
+
+// SocialContent
+import { SocialContent } from './SocialContent';
 
 @Component({
   // HTML
   selector: 'onePage-header',
-  
+
   // component: bağımsız çalışabilsin mi
   standalone: true,
 
@@ -20,6 +25,18 @@ import { Component } from '@angular/core';
 })
 export class OnepageHeaderComponent {
   // Field
+  header: string = "Angular Eğitimine Hoşgeldiniz";
+  title: string = "Html5,Css3, Javascript, Ecmascript, Typescript, Angular";
+
+  // Social 
+  socialContent: SocialContent[] = [
+    { id: 1, link: "http://link1", text: "linkedin", icon: "fa-brands fa-linkedin-in", classSocial: "btn btn-primary me-2" },
+    { id: 1, link: "http://link2", text: "gitHub", icon: "fa-brands fa-github", classSocial: "btn btn-danger me-2" },
+    { id: 1, link: "http://link3", text: "youtube", icon: "fa-brands fa-youtube", classSocial: "btn btn-secondary me-2" },
+    { id: 1, link: "http://link4", text: "blogger", icon: "fa-brands fa-blogger-b", classSocial: "btn btn-dark me-2" },
+  ];
+
   // Constructor
+  constructor() { }
   // Method
 }
